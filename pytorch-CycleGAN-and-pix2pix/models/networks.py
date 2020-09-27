813e3f827e9f35a88bc22869394eb86e0c56bf60
michelleappel <appel.michelle@gmail.com>
2020-09-27 17:48:55 +0200

    cycleGAN with batch weighting

--- pytorch-CycleGAN-and-pix2pix/models/networks.py
+++ pytorch-CycleGAN-and-pix2pix/models/networks.py
@@ -292,36 +292,80 @@ class GANLoss(nn.Module):
 
 class weighted_GANLoss(nn.Module):
     """Define different GAN objectives.
+
     The GANLoss class abstracts away the need to create the target label tensor
     that has the same size as the input.
     """
 
-    def __init__(self):
+    def __init__(self, gan_mode, target_real_label=1.0, target_fake_label=0.0):
         """ Initialize the GANLoss class.
+
         Parameters:
             gan_mode (str) - - the type of GAN objective. It currently supports vanilla, lsgan, and wgangp.
+            target_real_label (bool) - - label for a real image
+            target_fake_label (bool) - - label of a fake image
+
         Note: Do not use sigmoid as the last layer of Discriminator.
         LSGAN needs no sigmoid. vanilla GANs will handle it with BCEWithLogitsLoss.
         """
         super(weighted_GANLoss, self).__init__()
+        self.register_buffer('real_label', torch.tensor(target_real_label))
+        self.register_buffer('fake_label', torch.tensor(target_fake_label))
+        self.gan_mode = gan_mode
+        if gan_mode == 'lsgan':
+            self.loss = nn.MSELoss()
+        elif gan_mode == 'vanilla':
+            self.loss = nn.BCEWithLogitsLoss()
+        elif gan_mode in ['wgangp']:
+            self.loss = None
+        else:
+            raise NotImplementedError('gan mode %s not implemented' % gan_mode)
 
-    def L_minus(self, discriminated_A, weights):
-        return torch.sum(discriminated_A * 0.5*(1+weights))
+    def get_target_tensor(self, prediction, target_is_real):
+        """Create label tensors with the same size as the input.
 
-    def L_plus(self, discriminated_B, weights):
-        return torch.sum(discriminated_B * 0.5*(1+weights))
+        Parameters:
+            prediction (tensor) - - tpyically the prediction from a discriminator
+            target_is_real (bool) - - if the ground truth label is for real images or fake images
 
-    def loss_W(self, L_minus, L_plus):
-        """Compute loss for weight network"""
-        return abs(L_minus - L_plus)
+        Returns:
+            A label tensor filled with ground truth label, and with the size of the input
+        """
 
-    def loss_G(self, L_minus, L_plus):
-        """Compute loss for G network"""
-        return L_minus - L_plus
+        if target_is_real:
+            target_tensor = self.real_label
+        else:
+            target_tensor = self.fake_label
+        return target_tensor.expand_as(prediction)
 
-    def loss_D(self, L_minus, L_plus):
-        """Compute loss for D network"""
-        return - L_minus + L_plus
+    def __call__(self, prediction, target_is_real, weights=None):
+        """Calculate loss given Discriminator's output and grount truth labels.
+
+        Parameters:
+            prediction (tensor) - - tpyically the prediction output from a discriminator
+            target_is_real (bool) - - if the ground truth label is for real images or fake images
+
+        Returns:
+            the calculated loss.
+        """
+        if self.gan_mode in ['lsgan', 'vanilla']:
+            target_tensor = self.get_target_tensor(prediction, target_is_real)
+            if weights == None:
+                loss = self.loss(prediction, target_tensor)
+            else:
+                loss = ((prediction - target_tensor)*weights).sum()
+        elif self.gan_mode == 'wgangp':
+            if target_is_real:
+                if weights == None:
+                    loss = -prediction.mean()
+                else:
+                    loss = -(prediction*weights).sum()
+            else:
+                if weights == None:
+                    loss = prediction.mean()
+                else:
+                    loss = (prediction * weights).sum()
+        return loss
 
 class importance_sampling_GANLoss(nn.Module):
     """Define different GAN objectives.
