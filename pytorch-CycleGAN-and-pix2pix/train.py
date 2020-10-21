dacc07dac847acdb7adca110ae25713e48627560
Michelle <appel.michelle@gmail.com>
2020-10-21 19:54:36 +0200

    Update train.py

--- pytorch-CycleGAN-and-pix2pix/train.py
+++ pytorch-CycleGAN-and-pix2pix/train.py
@@ -96,6 +96,7 @@ if __name__ == '__main__':
                 visualizer.print_current_losses(epoch, epoch_iter, losses, t_comp, t_data)
                 if opt.display_id > 0:
                     visualizer.plot_current_losses(epoch, float(epoch_iter) / dataset_size, losses)
+                if opt.train_W:
                     visualizer.plot_current_losses(epoch, float(epoch_iter) / dataset_size, {'total_norm': total_norm})
                 
             if total_iters % opt.save_latest_freq == 0:   # cache our latest model every <save_latest_freq> iterations
